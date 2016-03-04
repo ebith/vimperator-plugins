fa1587d32413572c97464e09072b9a2480bd7a2f
ebith <ebith.h@gmail.com>
2016-03-05 01:32:10 +0900

    &だけエスケープ済みでやってくる

--- resources/twittperator-sidebar.js
+++ resources/twittperator-sidebar.js
@@ -32,7 +32,7 @@ window.addEventListener('load', () => {
       usernameIncludeSymbol: true,
       targetBlank: true,
     };
-    var escaped = _.escape(tweet.text);
+    var escaped = _.escape(tweet.text).replace(/&amp;amp;/, '&amp;');
     var autoLinked = twttr.txt.autoLink(escaped, options);
     tweet.linkedHtml = twemoji.parse(autoLinked);
 
